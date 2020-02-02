9eba4c64a0e876b96837b332e6d1198087c5abd9
shuvo575 <shohaghasan575@gmail.com>
2020-02-02 23:01:26 +0600

    Updated

--- js/init.js
+++ js/init.js
@@ -1,7 +1,34 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+var elem = document.querySelector('.tabs');
+var instance = M.Tabs.init(elem, {});
+var elem = document.querySelector('#tab2');
+var instance = M.Tabs.init(elem, {});
+
+
+//Get the button
+var mybutton = document.getElementById("back");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
